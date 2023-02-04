3c0f8fff1c02642a1ac19e63620bcd0191791735
maxiee <maxieewong@gmail.com>
2023-02-04 15:45:42 +0800

    create task

--- lib/kit/task/task/task_model.dart
+++ lib/kit/task/task/task_model.dart
@@ -1,21 +1,31 @@
 /// 任务
 class Task {
-  Task({required this.name, required this.finish, required this.created});
+  Task(
+      {required this.uuid,
+      required this.name,
+      required this.finish,
+      required this.created});
 
+  static const int kFinish = 1;
+  static const int kUnfinish = 0;
+
+  String uuid;
   String name;
-  bool finish;
+  int finish; // 0 unfinish, 1 finish
   DateTime created;
 
   Map<String, dynamic> toMap() {
     return {
+      'uuid': uuid,
       'name': name,
       'finish': finish,
       'created': created.millisecondsSinceEpoch
     };
   }
 
-  Task fromMap(Map<String, dynamic> map) {
+  static Task fromMap(Map<String, dynamic> map) {
     return Task(
+        uuid: map['uuid'],
         name: map['name'],
         finish: map['finish'],
         created: DateTime.fromMillisecondsSinceEpoch(map['created']));
---
+++ lib/kit/task/task/task_service.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+import 'package:punk_os/kit/task/task/task_model.dart';
+import 'package:ray_db/ray_db.dart';
+import 'package:uuid/uuid.dart';
+
+const String kCollectionNameTask = 'task';
+
+Task createTaskWithName(String name) {
+  return createTaskWithTask(Task(
+      uuid: const Uuid().v4(),
+      name: name,
+      finish: Task.kUnfinish,
+      created: DateTime.now()));
+}
+
+Task createTaskWithTask(Task t) {
+  GetIt.I.get<Database>().collection(kCollectionNameTask).storeMap(t.toMap());
+  return t;
+}
