f1fff447ca77f051adc2b5db79663a6b3375a5fc
maxiee <maxieewong@gmail.com>
2023-02-05 00:18:04 +0800

    待办任务列表

--- lib/kit/task/task/task_dashboard.dart
+++ lib/kit/task/task/task_dashboard.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:punk_os/kit/shell/dashboard/widget/dashboard_list.dart';
 import 'package:punk_os/kit/task/task/task_item.dart';
+import 'package:punk_os/kit/task/task/task_model.dart';
 import 'package:punk_os/kit/task/task/task_service.dart';
 
 DashboardList dashboardRecentTask(int limit) {
   return DashboardList(
-      title: "最近任务",
-      items: getRecentTasks(limit).map((e) => taskListTile(e)).toList());
+      title: "待办任务",
+      items: getRecentTasks(limit)
+          .where((e) => e.finish == Task.kUnfinish)
+          .map((e) => taskListTile(e))
+          .toList());
 }
